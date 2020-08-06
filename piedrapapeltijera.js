546f08693b84f2f9a7ddf77515c51e2d2a23fd92
britojesusr <britojesus931@gmail.com>
2020-08-05 23:24:03 -0400

    piedra_papel_tijera_0.1

---
+++ piedrapapeltijera.js
@@ -0,0 +1,70 @@
+function selectPiedra(){
+    document.getElementById('piedra').style.display='none';
+    
+}
+function selectPapel(){
+    document.getElementById('papel').style.display='none';
+    
+}
+function selectTijera(){
+    document.getElementById('tijera').style.display='none';
+    
+}
+
+
+
+
+
+
+/*
+
+function piedraPapelTijera() {
+ 
+    if (opcionUsuario == opcionPC) {
+
+        console.log("La opcion de ambos es: "+ opcionPC + " se ha producido un empate");
+}
+
+    else if (opcionUsuario=="papel" && opcionPC=="piedra") {
+
+        console.log("Ha ganado el usario");
+}
+
+    else if (opcionUsuario=="papel" && opcionPC=="tijera") {
+
+        console.log("Ha ganado la PC");
+}
+
+    else if (opcionUsuario=="piedra" && opcionPC=="tijera") {
+
+        console.log("Ha ganado el usuario");
+}
+
+ else if (opcionUsuario=="piedra" && opcionPC=="papel") {
+
+        console.log("Ha ganado la pc");
+}
+
+
+    else if (opcionUsuario=="tijera" && opcionPC=="papel") {
+
+        console.log("Ha ganado el usuario");
+}
+   
+
+    else if (opcionUsuario=="tijera" && opcionPC=="piedra") {
+
+        console.log("Ha ganado la pc");
+}
+
+    else {
+
+    console.log("Resultado desconocido, vuelva a elegir");
+
+}
+
+
+}
+
+piedraPapelTijera();
+*/
