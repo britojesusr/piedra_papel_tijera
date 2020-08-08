1d8e1f0ede22e5c89740155b85f95a2217e07245
britojesusr <britojesus931@gmail.com>
2020-08-08 03:39:21 -0400

    funcionalidad al 100%, falta contador

--- piedrapapeltijera.js
+++ piedrapapeltijera.js
@@ -7,7 +7,7 @@ var max = 3;
 
 function choicePlayer(seleccion){    
    
-    var seleccionJugador = document.getElementById('seleccionJugador');       
+    var seleccionJugador = document.getElementById('seleccionJugador');     
 
     if(seleccion===1) {
         seleccionJugador.setAttribute("src", "https://i.ibb.co/p2FKT1B/piedra.png");
@@ -15,17 +15,16 @@ function choicePlayer(seleccion){
         seleccionJugador.setAttribute("class", "piedra opcionJugador true");             
     }
 
-    else if(seleccion===2) {
+    else if (seleccion===2) {
         seleccionJugador.setAttribute("src", "https://i.ibb.co/2ggW1FD/papel.png");
         seleccionJugador.removeAttribute("class", "hidden");
-        seleccionJugador.setAttribute("class", "papel opcionJugador true");            
+        seleccionJugador.setAttribute("class", "papel opcionJugador true");        
     }
-
     else if (seleccion===3) {
         seleccionJugador.setAttribute("src", "https://i.ibb.co/qMkqkJn/tijera.png");
         seleccionJugador.removeAttribute("class", "hidden");
         seleccionJugador.setAttribute("class", "tijera opcionJugador true");        
-    }
+    }    
 
     else {
         alert("Debe seleccionar una opcion de ataque");
@@ -61,88 +60,107 @@ function choicePc(numRandom){
 
 //  Funcion  piedra papel o tijera batalla entre jugador y pc  
 
-function piedraPapelTijera(numRandom, seleccionJugador) {  
- 
-    var opcionUsuario = parametro1;
-    var opcionPC      = parametro2;
+function piedraPapelTijera(opcPlayer, numRandom) {
 
-    if (opcionUsuario == opcionPC) {
+    var textResul = document.getElementById('textResult');
+    
+    if (opcPlayer === numRandom) {
 
-        console.log("La opcion de ambos es: "+ opcionPC + " se ha producido un empate");
-}
+        textResul.textContent='Empate =)';
+        textResul.style.color='#06ffa1';
+    }
 
-    else if (opcionUsuario=="papel" && opcionPC=="piedra") {
+    else if(opcPlayer==1 && numRandom==2) {
 
-        console.log("Ha ganado el usario");
-}
+        textResul.textContent='paper covers rock';
+        textResul.style.color='#f64646';        
+        
+    }
 
-    else if (opcionUsuario=="papel" && opcionPC=="tijera") {
+    else if(opcPlayer==1 && numRandom==3) {
 
-        console.log("Ha ganado la PC");
-}
+        textResul.textContent='rock crushes scissors';
+        textResul.style.color='#cf1b1b';        
+        
+    }
+    else if(opcPlayer==2 && numRandom==1) {
 
-    else if (opcionUsuario=="piedra" && opcionPC=="tijera") {
+        textResul.textContent='paper covers rock';
+        textResul.style.color='#cf1b1b';        
+        
+    }
+    else if(opcPlayer==2 && numRandom==3) {
 
-        console.log("Ha ganado el usuario");
-}
+        textResul.textContent='scissors cuts paper';
+        textResul.style.color='#cf1b1b';        
+        
+    }
+    else if(opcPlayer==3 && numRandom==1) {
 
- else if (opcionUsuario=="piedra" && opcionPC=="papel") {
+        textResul.textContent='rock crushes scissors';
+        textResul.style.color='#cf1b1b';    
+    }
+    else if(opcPlayer==3 && numRandom==2) {
 
-        console.log("Ha ganado la pc");
-}
-
-
-    else if (opcionUsuario=="tijera" && opcionPC=="papel") {
-
-        console.log("Ha ganado el usuario");
-}
-   
-
-    else if (opcionUsuario=="tijera" && opcionPC=="piedra") {
-
-        console.log("Ha ganado la pc");
-}
-
-    else {
-
-    console.log("Resultado desconocido, vuelva a elegir");
-
-}
+        textResul.textContent='scissors cuts paper';
+        textResul.style.color='#cf1b1b';        
+    } 
 
 
 }
-
 
 //Funcion iniciar batalla - Se activa con el Boton battle del HTML y llama a piedraPapelTijera();
 
 function startBattle(){
 
-     if ( document.getElementById('seleccionJugador').classList.contains(true) ) {
+    var seleccionJugador = document.getElementById('seleccionJugador');
 
-        var numRandom = Math.floor(Math.random()*(max - min + 1)) + min;
-        console.log(min, max);
-        console.log(numRandom);  
-    
-        choicePc(numRandom);       
-        var opcPlayer = document.getElementById('seleccionJugador').classList.item(0);
+    if ( seleccionJugador.classList.contains(true)){
+
+        var numRandom = Math.floor(Math.random()*(max - min + 1)) + min;      
+        choicePc(numRandom);
+
+        var x = seleccionJugador.classList.item(0);
+            
+        console.log(x);
+
+        if ( x == "piedra") {
+            var opcPlayer = 1;
+            piedraPapelTijera(opcPlayer, numRandom);
+        }
+
+        else if (x == "papel") {
+            var opcPlayer = 2;            
+            piedraPapelTijera(opcPlayer, numRandom);
+        }
         
-        console.log("Funciona soy " + opcPlayer);    
-        console.log("La pc ataca con : " +numRandom);   
-    
-        piedraPapelTijera(numRandom, opcPlayer);
+        else {
+            var opcPlayer = 3;            
+            piedraPapelTijera(opcPlayer, numRandom);
+        }                      
 
-     } 
+    } 
     
-     else {
+    else {
         alert("Debe seleccionar una opcion de ataque");
-     }
-    
-
-        
-
+    }
 
 }
 
+
+function resetBattle(){
+    var seleccionJugador = document.getElementById('seleccionJugador');
+    var seleccionPc = document.getElementById('seleccionPc');
+    var textResul = document.getElementById('textResult');
+    
+    seleccionJugador.removeAttribute("src");
+    seleccionJugador.setAttribute("class", "hidden");
+    seleccionPc.removeAttribute("src");
+    seleccionPc.setAttribute("class", "hidden");
+    textResul.textContent='';
+    
+    empate
+}
 
 
 
