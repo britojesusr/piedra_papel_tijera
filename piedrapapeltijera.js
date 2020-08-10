12112a70d1888d213b920e6709670fa2da73370a
britojesusr <britojesus931@gmail.com>
2020-08-10 11:14:36 -0400

    funcionalidad optimizada

--- piedrapapeltijera.js
+++ piedrapapeltijera.js
@@ -1,7 +1,4 @@
-/* Valores de referencia
-    Rock = 1 
-    Paper = 2
-    Scissors = 3            */
+/*Valores de referencia Rock =1; Paper = 2; Scissors = 3 */
 
 // Rango de valores para la pc (1, 2, 3) (piedra, papel, tijera)
 
@@ -10,7 +7,7 @@ const max = 3;
 // contadores para el puntaje
 
     
-// Funcion para traer la opcion seleccionada por el jugador desde html
+// Toma la seleccion del jugador y ejecuta la funcion aleatoria para la seleccion de PC
 
 function choicePlayer(selection){    
    
@@ -19,18 +16,22 @@ function choicePlayer(selection){
     if(selection===1) {
         selectionPlayer.setAttribute("src", "https://i.ibb.co/p2FKT1B/piedra.png");
         selectionPlayer.removeAttribute("class", "hidden");
-        selectionPlayer.classList.add("piedra", "optionPlayer", "true");             
+        selectionPlayer.classList.add("piedra", "optionPlayer", "true");
+        startBattle();
     }
 
     else if (selection===2) {
         selectionPlayer.setAttribute("src", "https://i.ibb.co/2ggW1FD/papel.png");
         selectionPlayer.removeAttribute("class", "hidden");
-        selectionPlayer.classList.add("papel", "optionPlayer", "true");        
+        selectionPlayer.classList.add("papel", "optionPlayer", "true");
+        startBattle();   
     }
+
     else if (selection===3) {
         selectionPlayer.setAttribute("src", "https://i.ibb.co/qMkqkJn/tijera.png");
         selectionPlayer.removeAttribute("class", "hidden");
-        selectionPlayer.classList.add("tijera", "optionPlayer", "true");       
+        selectionPlayer.classList.add("tijera", "optionPlayer", "true");
+        startBattle();     
     }    
 
     else {
@@ -69,22 +70,18 @@ function choicePc(numRandom){
 
 function piedraPapelTijera(opcPlayer, numRandom) {
 
-
     const textResul = document.getElementById('textResult');
     const scorePlayer = document.getElementById('scorePlayer');
     const scorePc = document.getElementById('scorePc');
 
     let contPlayer = parseInt(scorePlayer.value); 
-    let contPc = parseInt(scorePc.value);
+    let contPc = parseInt(scorePc.value);  
     
-    console.log("Ahora parece que si " +contPlayer + contPc);
-         
         if (opcPlayer === numRandom) {
 
             textResul.textContent='tied';
             textResul.style.color='#06ffa1';
-            console.log(contPlayer, contPc);           
-            
+            winner();             
         }
 
         else if(opcPlayer==1 && numRandom==2) {
@@ -93,8 +90,7 @@ function piedraPapelTijera(opcPlayer, numRandom) {
             textResul.style.color='#f64646';
             contPc+=1;
             document.getElementById('scorePc').value=contPc;
-            console.log(contPlayer, contPc); 
-                                        
+            winner();                                       
         }
 
         else if(opcPlayer==1 && numRandom==3) {
@@ -103,8 +99,7 @@ function piedraPapelTijera(opcPlayer, numRandom) {
             textResul.style.color='#f64646';
             contPlayer+=1;
             document.getElementById('scorePlayer').value=contPlayer;
-            console.log(contPlayer, contPc);
-                          
+            winner();                          
         }
 
         else if(opcPlayer==2 && numRandom==1) {
@@ -113,82 +108,52 @@ function piedraPapelTijera(opcPlayer, numRandom) {
             textResul.style.color='#f64646';
             contPlayer+=1;
             document.getElementById('scorePlayer').value=contPlayer;
-            console.log(contPlayer, contPc);                     
-            
+            winner();              
         }
+
         else if(opcPlayer==2 && numRandom==3) {
 
             textResul.textContent='scissors cuts paper';
             textResul.style.color='#f64646';
             contPc+=1;
             document.getElementById('scorePc').value=contPc;
-            console.log(contPlayer, contPc);             
-                     
+            winner();           
         }
+
         else if(opcPlayer==3 && numRandom==1) {
 
             textResul.textContent='rock crushes scissors';
             textResul.style.color='#f64646';
             contPc+=1;
             document.getElementById('scorePc').value=contPc;
-            console.log(contPlayer, contPc);              
-            
+            winner(); 
         }
+
         else if(opcPlayer==3 && numRandom==2) {
 
             textResul.textContent='scissors cuts paper';
             textResul.style.color='#f64646';
             contPlayer+=1;
             document.getElementById('scorePlayer').value=contPlayer;
-            console.log(contPlayer, contPc);                    
-                         
+            winner();                                
         }    
-   
 }
 
 //Funcion iniciar batalla - Se activa con el Boton battle del HTML y llama a piedraPapelTijera();
 
 function startBattle(){
 
-    const playerWin = document.getElementById('playerWin');
-    const pcWin = document.getElementById('pcWin');
+    const selectionPlayer = document.getElementById('selectionPlayer');
+    const selectionPlayerValue =selectionPlayer.classList;
 
-    const contPlayer = document.getElementById('scorePlayer');
-    const contPlayerValue = contPlayer.value;
+    if ( selectionPlayerValue.contains(true)){
+        
+        document.getElementById('options').style.pointerEvents="none";
+                
+        let numRandom = Math.floor(Math.random()*(max - min + 1)) + min;      
+        choicePc(numRandom);
 
-    const contPc = document.getElementById('scorePc');
-    const contPcValue = contPc.value;
-
-    console.log(contPlayerValue, contPcValue);
-
-    if (contPlayerValue==10) {
-        playerWin.textContent='YOU WIN';
-        playerWin.style.color='#00FF00';
-        alert("YOU WIN");
-        resetAllBattle();
-    }
-
-    else if (contPcValue==10){        
-        pcWin.textContent='Javascript Win';
-        pcWin.style.color='#00FF00';
-        playerWin.textContent='You Lose';
-        playerWin.style.color='#FF0000';
-        alert("PC WIN");
-        resetAllBattle();
-    }    
-
-    else { 
-
-        const selectionPlayer = document.getElementById('selectionPlayer');
-        const selectionPlayerValue =selectionPlayer.classList;
-
-        if ( selectionPlayerValue.contains(true)){          
-
-            let numRandom = Math.floor(Math.random()*(max - min + 1)) + min;      
-            choicePc(numRandom);
-
-            let x = selectionPlayer.classList.item(0);                
-            
+        let x = selectionPlayer.classList.item(0);              
 
             if ( x == "piedra") {
                 let opcPlayer = 1;
@@ -200,52 +165,48 @@ function startBattle(){
                 piedraPapelTijera(opcPlayer, numRandom);
             }
             
-            else {
+            else if ( x == "tijera") {
                 let opcPlayer = 3;            
                 piedraPapelTijera(opcPlayer, numRandom);
-            }                      
+            }
 
-        } 
-        
-        else {
-            alert("Debe seleccionar una opcion de ataque");
-        }
+            else  {
+                alert("Se ha roto el codigo, por favor reset");
+            }      
+    } 
+    
+    else {
+        alert("Debe seleccionar una opción");
     }
+    
+}   
 
-}
-
-function continueBattle() {
+function nextRound() {
        
     const selectionPlayer = document.getElementById('selectionPlayer');
     const selectionPc = document.getElementById('selectionPc');
     const textResul = document.getElementById('textResult');
-
-    const selectionPcValue=selectionPc.classList;
-
-    if (selectionPcValue.contains(true)) {  
+    
+    document.getElementById('options').style.pointerEvents="initial";
 
     selectionPlayer.removeAttribute("src");
     selectionPlayer.setAttribute("class", "hidden");
     selectionPc.removeAttribute("src");
     selectionPc.setAttribute("class", "hidden"); 
     textResul.textContent='';
-
-    }
-
-    else {
-        alert("First Start Battle");
-    }    
 }
-
-
 
 function resetAllBattle(){
     const selectionPlayer = document.getElementById('selectionPlayer');
     const selectionPc = document.getElementById('selectionPc');
     const textResul = document.getElementById('textResult');
     const contPlayer = document.getElementById('scorePlayer');
-    const contPc = document.getElementById('scorePc');  
-    
+    const contPc = document.getElementById('scorePc');
+    const playerWin = document.getElementById('playerWin');
+    const pcWin = document.getElementById('pcWin');
+
+    playerWin.textContent='';
+    pcWin.textContent='';
     selectionPlayer.removeAttribute("src");
     selectionPlayer.setAttribute("class", "hidden");
     selectionPc.removeAttribute("src");
@@ -253,9 +214,31 @@ function resetAllBattle(){
     contPlayer.value=0;
     contPc.value=0;
     textResul.textContent='';   
-    
 }
 
+function winner () {
 
+    const playerWin = document.getElementById('playerWin');
+    const pcWin = document.getElementById('pcWin');
 
+    const contPlayer = document.getElementById('scorePlayer');
+    const contPlayerValue = contPlayer.value;
+
+    const contPc = document.getElementById('scorePc');
+    const contPcValue = contPc.value;   
+
+    if (contPlayerValue==10) {
+        playerWin.textContent='YOU WIN';
+        playerWin.style.color='#00FF00';        
+        document.getElementById('nextButton').style.pointerEvents="none";
+    }
+
+    else if (contPcValue==10){        
+        pcWin.textContent='Javascript Win';
+        pcWin.style.color='#00FF00';
+        playerWin.textContent='You Lose';
+        playerWin.style.color='#FF0000';        
+        document.getElementById('nextButton').style.pointerEvents="none";
+    }  
+}
 
